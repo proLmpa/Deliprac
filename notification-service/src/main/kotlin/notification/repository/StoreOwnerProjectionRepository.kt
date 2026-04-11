1bfaeac8fbe397099f5fbe75a36fc715e06b4902
proLmpa <kheeyeoul@gmail.com>
2026-04-11 22:32:16 +0900

    Remove all Kafka/WebSocket remnants from notification-service
    Deleted:
    - WebSocketConfig.kt
    - OrderEventConsumer.kt
    - StoreEventConsumer.kt
    - NotificationWebSocketHandler.kt
    - StoreOwnerProjection.kt (Kafka-era entity, unused)
    - StoreOwnerProjectionRepository.kt (Kafka-era repository, unused)
    
    Notification delivery is now handled synchronously by the BFF via HTTP POST /internal/notifications.

--- notification-service/src/main/kotlin/notification/repository/StoreOwnerProjectionRepository.kt
+++
@@ -1,6 +0,0 @@
-package notification.repository
-
-import notification.entity.StoreOwnerProjection
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface StoreOwnerProjectionRepository : JpaRepository<StoreOwnerProjection, Long>
